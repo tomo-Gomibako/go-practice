70755938c769d6639ef933b9d42cf46cf33b0eca
tomon9086 <tomo.mouse.9086@gmail.com>
2020-02-23 19:22:02 +0900

    ファイル分割間違ってた

--- input/string.go
+++ input/string.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 )
 
@@ -19,32 +18,4 @@ func String() {
 	}
 	text = strings.ToUpper(string(text[0])) + text[1:]
 	fmt.Println("Hello, " + text + ".")
-}
-
-// Int int input test
-func Int() {
-	stdin := bufio.NewScanner(os.Stdin)
-	fmt.Println("Type a number.")
-	stdin.Scan()
-	text := stdin.Text()
-	i, _ := strconv.Atoi(text)
-	fmt.Println("Next is " + strconv.Itoa(i+1) + ".")
-}
-
-// SplitInt splitted int input test
-func SplitInt() {
-	stdin := bufio.NewScanner(os.Stdin)
-	fmt.Println("Type numbers separated with space.")
-	stdin.Scan()
-	text := stdin.Text()
-	strs := strings.Fields(text)
-	// strs := strings.Split(text, " ")
-	ints := []int{}
-	for _, c := range strs {
-		i, err := strconv.Atoi(c)
-		if err == nil {
-			ints = append(ints, i)
-		}
-	}
-	fmt.Println(ints)
 }
